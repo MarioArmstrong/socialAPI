f196b774bbebeb328daf97c2fce7f72046514782
MarioArmstrong <arms.mario@icloud.com>
2022-10-27 09:35:45 -0700

    working on routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,55 @@
+const { ObjectId } = require("mongoose").Types;
+const { User } = require("../models");
 
+module.exports = {
+    getAllUser(req,res) {
+        try{
+            User.find()
+            .then(async(users) => {
+                const userObj = {
+                    users 
+                };
+                return res.json(userObj);
+            })
+        }catch(err){
+            console.log(err);
+            return res.status(500).json(err);
+        };
+    },
+
+    getSingleUser(req,res) {
+        try{
+        User.findOne({_id: req.params.userId})
+            .select("-__v")
+                .then(async(user) =>{
+               if(!user) {
+                res.status(404).json({message: "No user with that ID"})
+               } else {
+                res.json({student})
+               }
+            }}catch(err){
+                console.log(err);
+                return res.status(500).json(err);
+                }
+    },
+
+    createUser(req, res) {
+        User.create(req.body)
+          .then((user) => res.json(user))
+          .catch((err) => res.status(500).json(err));
+      },
+
+      deleteUser(req, res) {
+        User.findOneAndRemove({ _id: req.params.userId })
+          .then((user) =>
+            if(!user){
+            res.status(404).json({ message: "No such user exists" })
+            }else{
+                return res.json({message: "User was deleted"});
+            }
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+      },
+}
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,1 +1,15 @@
+const router = require('express').Router();
 
+const {
+    getAllUser,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+} = require("../../userController");
+
+router.route('/').get(getAllUser).post(createUser);
+
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+module.exports = router;
